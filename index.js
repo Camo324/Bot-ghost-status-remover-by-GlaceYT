e44ca2b2be99500f9e07a0f6738bc58c3cba9cfb
Camo324 <168047227+Camo324@users.noreply.github.com>
2024-04-25 08:51:25 -0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Watching Avalon Interactive","Watching General"];
 
 
 let currentIndex = 0;
